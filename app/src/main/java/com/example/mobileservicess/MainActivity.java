eb8b7cb7990574191b45c2420c98236012d9e5bb
PetrovskaMihaela <mihaela.petrovska2@gmail.com>
2021-05-12 17:34:55 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mobileservicess/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.mobileservicess;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            RestartServiceBroadcastReceiver.scheduleJob(getApplicationContext());
+        } else {
+            ProcessMainClass bck = new ProcessMainClass();
+            bck.launchService(getApplicationContext());
+        }
+        finish();
+    }
+}
